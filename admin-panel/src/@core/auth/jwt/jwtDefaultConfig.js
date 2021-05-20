a347c0376846ccfd575a2bf750d2d8b9ad412411
Anmol Srivastava <anmol150srivastava@gmail.com>
2021-05-21 01:43:01 +0530

    admin-panel updates

--- admin-panel/src/@core/auth/jwt/jwtDefaultConfig.js
+++ admin-panel/src/@core/auth/jwt/jwtDefaultConfig.js
@@ -1,9 +1,13 @@
+const { backend } = require('../../../global-vars')
+
 export default {
   // Endpoints
-  loginEndpoint: '/jwt/login',
-  registerEndpoint: '/jwt/register',
-  refreshEndpoint: '/jwt/refresh-token',
-  logoutEndpoint: '/jwt/logout',
+  loginEndpoint: `${backend}/login`,
+  // loginEndpoint: '/jwt/login',
+  registerEndpoint: `${backend}/register`,
+  // registerEndpoint: '/jwt/register',
+  refreshEndpoint: `${backend}/token`,
+  logoutEndpoint: `${backend}/logout`,
 
   // This will be prefixed in authorization header with token
   // e.g. Authorization: Bearer <token>
